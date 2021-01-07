99c8f5bf8c9ba864c7e158827c313237dd97eac2
JMiguel0 <jarchundiah@toluca.tecnm.mx>
2021-01-07 17:13:40 -0600

    Ya tiene gps el cliente pero no muestra nada

--- app/src/main/java/com/example/movl_proyectofinal/cliente/gps/gps.java
+++ app/src/main/java/com/example/movl_proyectofinal/cliente/gps/gps.java
@@ -1,7 +1,10 @@
 package com.example.movl_proyectofinal.cliente.gps;
 
+import androidx.fragment.app.FragmentActivity;
 import androidx.lifecycle.ViewModelProviders;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,28 +14,57 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.movl_proyectofinal.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class gps extends Fragment {
+    public class gps extends Fragment {
+        private GpsViewModel mViewModel;
 
-    private GpsViewModel mViewModel;
 
-    public static gps newInstance() {
-        return new gps();
+        public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                                 @Nullable Bundle savedInstanceState) {
+            final View root = inflater.inflate(R.layout.fragment_gps, container, false);
+            Button btn = root.findViewById(R.id.btn);
+            btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    abrir(view);
+                }
+            });
+            return root;
+
+
+
+
+
+            //btEntrar.setOnClickListener(new View.OnClickListener() {
+
+            //  @Override
+            //public void onClick(View v) {
+            //  Intent i;
+            //i = new Intent(getContext(), MapsActivity.class);
+            //startActivity(i);
+
+
+            //}
+            //});
+        }
+
+
+
+        public void abrir (View view){
+
+            Intent abrir = new Intent(getContext(),MapsActivity.class) ;
+            startActivity(abrir);
+
+        }
+
+
     }
-
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_gps, container, false);
-    }
-
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = ViewModelProviders.of(this).get(GpsViewModel.class);
-        // TODO: Use the ViewModel
-    }
-
-}
